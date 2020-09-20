58136bf3fb0253c916a09678a1935fba6bad2052
parasvcb <parasvcb@gmail.com>
2020-09-21 00:25:54 +0530

    module wise from tunnel menton to tyrone sep 21 (new changes in plotting codes

--- barplot_hbatpeaks.R
+++ barplot_hbatpeaks.R
@@ -16,20 +16,88 @@ drops <- c("datatype")
 dfgen<-dfgen[ , !(names(dfgen) %in% drops)]
 
 #print (head(dfgen))
-dfgen$hbfrom <- ifelse(grepl('all',dfgen$category), '1_all', '2_adj')
+dfgen$hbfrom <- ifelse(grepl('all',dfgen$category), 'nonadj', 'adj')
 dfgen$hbnature <- ifelse(grepl('stoch',dfgen$category), 'stochastic',
                   ifelse(grepl('stab',dfgen$category), 'stable',   'raw'))
-dfgen$hbx <- paste(dfgen$hbnature,'_',dfgen$hbfrom)
+# this should be segregated such that, raw information will be available in one plot (df)
+# stab and stoch will go together for adj and nondadj so in one df (plot) 
+
+dfgen_primary = dfgen[(dfgen$hbnature=='raw'),]
+dfgen_primary$hbx <- paste(dfgen_primary$polymer,'_',dfgen_primary$hbfrom)
+
+#dfgen_secondary_adj = dfgen[(dfgen$hbfrom=='adj') & !(dfgen$hbnature=='raw'),]
+#dfgen_secondary_nonadj = dfgen[(dfgen$hbfrom=='nonadj') & !(dfgen$hbnature=='raw'),]
+dfgen_secondary_comp = dfgen[!(dfgen$hbnature=='raw'),]
+dfgen_secondary_comp$facetvar <- paste(dfgen_secondary_comp$hbfrom,'_',dfgen_secondary_comp$hbnature)
+dfgen_secondary_comp$hbx <- dfgen_secondary_comp$polymer
+
+
 #print (head(dfgen))
 
-gg <- ggplot(data=dfgen,aes(x=hbx,y=mean,fill=hbtype))
-gg <- gg + geom_bar(stat="identity",position=position_dodge())
-gg <- gg + geom_text(aes(label=mean),vjust=1, color="black",position = position_dodge(0.9), size=3)
-gg <- gg + geom_errorbar(data= dfgen, aes(x=hbx,y=mean,ymin=mean-std, ymax=mean+std),position = position_dodge(0.9),size=0.3,width=0.5)
-gg <- gg + theme (axis.text.x = element_text( hjust = 1, size = 7, angle = 45),axis.text.y = element_text( hjust = 1, size = 7), legend.position="top", panel.background = element_rect(fill = "white", colour = "grey50"),panel.grid.major = element_line(colour = "grey90"),panel.grid.minor = element_line(colour = "grey95",size = 0.25))
-gg <- gg + facet_wrap(~polymer,ncol=1,scale='free_x') 
-ggsave(filename = outputfile, height=15, width=5)
+#Because of vertical stacking, need to add the means such that SCSC=SCSC, MCSC=SCSC+MCSC, MCMC=SCSC+MCSC+MCMC
+# now Upp_lim will be different SCSC=SCSC, MCSC=mean_MCSC-std, MCMC=mean_MCMC-std
+# and Low_lim SCSC=SCSC, MCSC=mean_MCSC-std, MCMC=mean_MCMC-std
+dfgen_primary$vertmean = dfgen_primary$mean
 
+# dfgen_primary$vertmean[dfgen_primary$hbtype == "mcmc"] <- with (dfgen_primary, mean[dfgen_primary$hbtype == "mcmc"] + mean[dfgen_primary$hbtype == "mcsc"] + mean[dfgen_primary$hbtype == "scsc"])
+# dfgen_primary$vertmean[dfgen_primary$hbtype == "mcsc"] <- with (dfgen_primary, mean[dfgen_primary$hbtype == "mcsc"] + mean[dfgen_primary$hbtype == "scsc"])
+# dfgen_primary$vertmean[dfgen_primary$hbtype == "scsc"] <- with (dfgen_primary, mean[dfgen_primary$hbtype == "scsc"])
+
+# the above and below values are nothing but tweaks to get the ordering wise std dev bars to appear, cant add text in betwen as of now.
+dfgen_primary$vertmean[dfgen_primary$hbtype == "scsc"] <- with (dfgen_primary, mean[dfgen_primary$hbtype == "mcmc"] + mean[dfgen_primary$hbtype == "mcsc"] + mean[dfgen_primary$hbtype == "scsc"])
+dfgen_primary$vertmean[dfgen_primary$hbtype == "mcsc"] <- with (dfgen_primary, mean[dfgen_primary$hbtype == "mcsc"] + mean[dfgen_primary$hbtype == "mcmc"])
+dfgen_primary$vertmean[dfgen_primary$hbtype == "mcmc"] <- with (dfgen_primary, mean[dfgen_primary$hbtype == "mcmc"])
+
+
+#print (dfgen_primary)
+gg <- ggplot(data=dfgen_primary,aes(x=hbx,y=mean,fill=hbtype))
+gg <- gg + geom_bar(position="stack", stat="identity") + geom_col(position = position_stack(reverse = TRUE))
+gg <- gg + scale_fill_manual(values=c("gray","green","red"))
+#gg <- gg + geom_text(aes(label=mean), position = position_stack(vjust = 0.5)) #vjust=1, color="black", size=1)
+gg <- gg + geom_errorbar(aes(x=hbx,y=vertmean,ymin=vertmean-std, ymax=vertmean+std,color = hbtype, width=0.3),position="identity") #+ geom_col(position = position_stack(reverse = TRUE))
+gg <- gg + scale_color_manual(values=c("brown","black","blue"))
+gg <- gg + ylab("Hb-count")
+gg <- gg + xlab("Polymer")
+gg <- gg + theme (axis.text.x = element_text( hjust = 1, size = 7, angle = 45),axis.text.y = element_text( hjust = 1, size = 7), legend.position="top", panel.background = element_rect(fill = "white", colour = "grey50"),panel.grid.major = element_line(colour = "grey90"),panel.grid.minor = element_line(colour = "grey95",size = 0.25))
+ggsave(filename = paste(outputfile,'_','raw_plot.pdf'))
+
+gg <- ggplot(data=dfgen_primary,aes(x=polymer,y=mean,fill=hbtype))
+gg <- gg + geom_bar(position="stack", stat="identity") + geom_col(position = position_stack(reverse = TRUE))
+gg <- gg + scale_fill_manual(values=c("gray","green","red"))
+#gg <- gg + geom_text(aes(label=mean), position = position_stack(vjust = 0.5)) #vjust=1, color="black", size=1)
+gg <- gg + geom_errorbar(aes(x=polymer,y=vertmean,ymin=vertmean-std, ymax=vertmean+std,color = hbtype, width=0.3),position="identity") #+ geom_col(position = position_stack(reverse = TRUE))
+gg <- gg + scale_color_manual(values=c("brown","black","blue"))
+gg <- gg + ylab("Hb-count")
+gg <- gg + xlab("Polymer")
+gg <- gg + facet_wrap(~hbfrom,nrow=1)
+gg <- gg + theme (axis.text.x = element_text( hjust = 1, size = 7, angle = 45),axis.text.y = element_text( hjust = 1, size = 7), legend.position="top", panel.background = element_rect(fill = "white", colour = "grey50"),panel.grid.major = element_line(colour = "grey90"),panel.grid.minor = element_line(colour = "grey95",size = 0.25))
+ggsave(filename = paste(outputfile,'_','raw_plot_facet.pdf'))
+
+
+dfgen_secondary_comp$vertmean[dfgen_secondary_comp$hbtype == "scsc"] <- with (dfgen_secondary_comp, mean[dfgen_secondary_comp$hbtype == "mcmc"] + mean[dfgen_secondary_comp$hbtype == "mcsc"] + mean[dfgen_secondary_comp$hbtype == "scsc"])
+dfgen_secondary_comp$vertmean[dfgen_secondary_comp$hbtype == "mcsc"] <- with (dfgen_secondary_comp, mean[dfgen_secondary_comp$hbtype == "mcsc"] + mean[dfgen_secondary_comp$hbtype == "mcmc"])
+dfgen_secondary_comp$vertmean[dfgen_secondary_comp$hbtype == "mcmc"] <- with (dfgen_secondary_comp, mean[dfgen_secondary_comp$hbtype == "mcmc"])
+
+#print (dfgen_secondary_comp)
+
+gg <- ggplot(data=dfgen_secondary_comp,aes(x=hbx,y=mean,fill=hbtype))
+gg <- gg + geom_bar(position="stack", stat="identity") + geom_col(position = position_stack(reverse = TRUE))
+gg <- gg + scale_fill_manual(values=c("gray","green","red"))
+#gg <- gg + geom_text(aes(label=mean), position = position_stack(vjust = 0.5)) #vjust=1, color="black", size=1)
+gg <- gg + geom_errorbar(data= dfgen_secondary_comp, aes(x=hbx,y=vertmean,ymin=vertmean-std, ymax=vertmean+std,color = hbtype, width=0.3),position="identity") #+ geom_col(position = position_stack(reverse = TRUE))
+gg <- gg + scale_color_manual(values=c("brown","black","blue"))
+gg <- gg + facet_wrap(~facetvar,ncol=2,nrow=2)
+gg <- gg + ylab("Hb-count")
+gg <- gg + xlab("Polymer")
+gg <- gg + theme (axis.text.x = element_text( hjust = 1, size = 7, angle = 45),axis.text.y = element_text( hjust = 1, size = 7), legend.position="top", panel.background = element_rect(fill = "white", colour = "grey50"),panel.grid.major = element_line(colour = "grey90"),panel.grid.minor = element_line(colour = "grey95",size = 0.25))
+ggsave(filename = paste(outputfile,'_','segregated_plot.pdf'))
 }
-plothbdata("../data_tsv/out_hbonds_peak_1.tsv","../plots/new4sep/peak1_hbdata.pdf")
-plothbdata("../data_tsv/out_hbonds_peak_2.tsv","../plots/new4sep/peak2_hbdata.pdf")
+plothbdata("../data_tsv/out_hbonds_peak_1.tsv","../plots/new4sep/peak1_hbdata")
+plothbdata("../data_tsv/out_hbonds_peak_2.tsv","../plots/new4sep/peak2_hbdata")
+
+
+# going to reformat the plotting style
+# adjacent and non-adjacent will go as one in barplot, with stacking of different scsc and other types
+# For each adj and non adj, there will be supplimnetarty stochastic and stable plots, [lay together], and facet them 
+# top and bottom,
+# Each column will have color bar as mean and std dev together 
--- vsdisp_hbondcount_aspirations.R
+++ vsdisp_hbondcount_aspirations.R
@@ -3,15 +3,12 @@ args = commandArgs(trailingOnly=TRUE)
 if (length(args)<2) {
   stop("Please supply two arguements systemDir and output_append_withoutext", call.=FALSE)
 } else if (length(args)==2) {
-  # default output file
   systemFile=args[1]
   out=args[2]
 }
 library(ggplot2)
 library(reshape)
-
 directories=list.dirs(path = systemFile, full.names = TRUE, recursive=F)
-
 cropDF <- function(dfpre,polymer) {
     dffile="processed/dataframe_vnew2.tsv"
     dfadd=file.path(dfpre,polymer,dffile)
@@ -25,7 +22,9 @@ cropDF <- function(dfpre,polymer) {
     afterpre$polymer <- polymer
     return (afterpre)
 }
-#pullinginterctaion
+#rename the '-' containing variable to other so that it can be dropped later,
+#melt using the displacement, and categorise variable types in different columns 
+
 resname=paste0(out,"resdf.csv")
 if (file.exists(resname)){
     print('reading')
@@ -33,6 +32,7 @@ if (file.exists(resname)){
 } else {
 dfgen=cropDF(systemFile,"polyglycine")
 dfgen = dfgen[FALSE,]
+#above two statements will get me a empty df with only column names, but bit costly
 for (i in directories) {
     print (i)
     #print (systemFile)
@@ -44,34 +44,28 @@ for (i in directories) {
         #this function will get the dataframes appended together
  }
 }
+#The above block will iterate all the polymers in one replicate and if of desired type, will concatenate them
+#write so that this shouldnt be repeated, or remove wirtten file if raw file data has changed
 write.csv(dfgen,resname,sep = "\t")
 }
 print ('read')
 
 plotallad <- function(df,extractele,outapp) {
-    print ('0')
-    #print (unique(df$variable))
     df$keeps <- ifelse (grepl(extractele,df$variable),'keep',
     ifelse (grepl("for-dispav",df$variable),'keep','discard'))
-    print ('1')
-    #print (unique(df$keeps))
-    #print(head(df))
     df$hbcolor <- ifelse(grepl('mcmc',df$variable), 'mcmc',
                    ifelse(grepl('scsc',df$category), 'scsc',
                 ifelse(grepl('mcsc',df$category), 'mcsc',   'force')))
     dfset=df[(df$keeps=='keep'),]
+    dfForce=dfset[(dfset$variable=='for-dispav'),]
     dfset=dfset[!(dfset$variable=='for-dispav'),]
     #the above can be removed to add thin layout to force in bacground
     
     print ("here")
     #dfset=dfset[(dfset$avtype=='ra20'),]
     dfset=dfset[(dfset$avtype=='dispav'),]
-    print ('2')
-    #print (head(dfset))
-    #print (unique(dfset$variable))
     dfset$hbnature<- ifelse(grepl('p1',dfset$variable), 'partial',
                    ifelse(grepl('p2',dfset$variable), 'partial',   'unbiased'))
-    print ('3')
     dfset2=dfset[(dfset$hbnature=='partial'),]
     dfset1=dfset[(dfset$hbnature=='unbiased'),]
     
@@ -79,19 +73,20 @@ plotallad <- function(df,extractele,outapp) {
     # print (unique(dfset1$value))
     # write.csv(dfset1,'check.tsv',sep = "\t")
 
-    print (unique(dfset1$color))
+    print (unique(dfset1$polymer))
     print ('4')
-    #gg <- ggplot(data=dfraw[!is.na(dfraw$value),], aes(x=displacement,y=value,color=homopolymer))
+    maxval=max(dfset1$value,na.rm = TRUE)
+    coeff=round(maxval / 3,digits=2)
     gg <- ggplot(data=dfset1[!is.na(dfset1$value),], aes(x=displacement,y=value,color=hbcolor))
-    #gg <- gg + geom_line(size=0.6, ) + scale_color_manual(values=c("brown","black","darkgreen","red","blue","darkorange","azure4"))
-    gg <- gg + geom_line(size=0.6, ) + scale_color_manual(values=c("blue","red","green"))
-
+    gg <- gg + geom_line(size=0.4, alpha=0.8 )
+    gg <- gg + geom_line(data=dfForce[!is.na(dfForce$value),], aes(x=displacement, y=(value / 1000)*coeff), size=0.1, alpha=0.8)
+    gg <- gg + scale_color_manual(values=c("red","blue","black","green"))
+    gg <- gg + scale_y_continuous(name='Hb_count',sec.axis = sec_axis(~./coeff, name="Force (nN)")) 
     gg <- gg + facet_wrap(~polymer,ncol=1) 
     gg <- gg + scale_x_continuous(name="Displacement (Ang)",breaks=seq(-2,25,4))
     gg <- gg + theme (strip.text.x = element_text(size = 7),axis.text.x = element_text( hjust = 1, size = 7, angle = 45),axis.text.y = element_text( hjust = 1, size = 7), legend.position="top", panel.background = element_rect(fill = "white", colour = "grey50"),panel.grid.major = element_line(colour = "grey90"),panel.grid.minor = element_line(colour = "grey95",size = 0.25))
-    gg <- gg + guides(colour = guide_legend(override.aes = list(size=5)))
-    gg <- gg + ylab("hbcount")
-    ggsave(filename = paste0(outapp,'unbiased.pdf'),gg, height = 8, width= 3)
+    gg <- gg + guides(colour = guide_legend(override.aes = list(size=1)))
+    ggsave(filename = paste0(outapp,'unbiased.pdf'),gg, height = 10, width= 5)
 
     d1='dashed'
     d2='solid'
@@ -99,7 +94,7 @@ plotallad <- function(df,extractele,outapp) {
     c11='lightblue'
     c2='black'
     c21='darkgray'
-    c3='red'
+    c3='pink'
     c31='maroon'
     c4='orange'
     c41='yellow'
@@ -108,58 +103,72 @@ plotallad <- function(df,extractele,outapp) {
     c6='purple'
     c61='cyan'
     print (unique(dfset2$category))
-    gg <- ggplot(data=dfset2[!is.na(dfset2$value),], aes(x=displacement,y=value,color=category))
-    #gg <- gg + geom_line(size=0.6, ) + scale_color_manual(values=c("brown","black","darkgreen","red","blue","darkorange","azure4"))
-    gg <- gg + geom_line(size=0.4,) 
-    #gg <- gg + scale_linetype_manual(values=c(d1,d2,d1,d2,d1,d2,d1,d2,d1,d2,d1,d2))
-    gg <- gg + scale_color_manual(values=c(c1,c11,c2,c21,c3,c31,c1,c11,c2,c21,c3,c31))
-    gg <- gg + facet_wrap(~polymer,ncol=1) 
+    maxval=max(dfset2$value,na.rm = TRUE)
+    coeff=round(maxval / 3,digits=2)
+    #data$PROFILE <- sub("^([a-z]+[A-B]).*", "\\1", data$PROFILE)
+    #dfset2$category <- sub("all", "nonadj", dfset2$category)
+    dfset2$category <- sub("hball", "", dfset2$category)
+    dfset2$category <- sub("hbadj", "", dfset2$category)
+    
+    gg <- ggplot()
+    gg <- gg + geom_line(data=dfset2[!is.na(dfset2$value),], aes(x=displacement,y=value,color=category),size=0.4, alpha=0.8) 
+    gg <- gg + geom_line(data=dfForce[!is.na(dfForce$value),], aes(x=displacement, y=(value / 1000)*coeff), size=0.1, alpha=0.8)
+    gg <- gg + scale_color_manual(values=c("red",c1,c11,c2,c21,c3,c31,c1,c11,c2,c21,c3,c31))
+    gg <- gg + scale_y_continuous(name='Hb_count',sec.axis = sec_axis(~./coeff, name="Force (nN)")) 
     gg <- gg + scale_x_continuous(name="Displacement (Ang)",breaks=seq(-2,25,4))
+    gg <- gg + facet_wrap(~polymer,ncol=1) 
     gg <- gg + theme (strip.text.x = element_text(size = 7),axis.text.x = element_text( hjust = 1, size = 7, angle = 45),axis.text.y = element_text( hjust = 1, size = 7), legend.position="top", panel.background = element_rect(fill = "white", colour = "grey50"),panel.grid.major = element_line(colour = "grey90"),panel.grid.minor = element_line(colour = "grey95",size = 0.25))
-    #gg <- gg + guides(colour = guide_legend(override.aes = list(size=5)))
-    gg <- gg + ylab("hbcount")
-    ggsave(filename = paste0(outapp,'biased.pdf'),gg, height = 8, width= 3)
-
+    gg <- gg + guides(colour = guide_legend(override.aes = list(size=0.2)))
+    ggsave(filename = paste0(outapp,'biased1.pdf'),gg, height = 10, width= 5)
     }
     print ('5')
 plotcombined <- function(df,extractele,outapp) {
     print ('0')
-    #print (unique(df$variable))
     df$keeps <- ifelse (grepl(extractele,df$variable),'keep',
     ifelse (grepl("for-dispav",df$variable),'keep','discard'))
-    df$hbcolor <- ifelse(grepl('adjmcmc',df$variable), 'adjmcmc',
-                   ifelse(grepl('adjscsc',df$category), 'adjscsc',
-                ifelse(grepl('adjmcsc',df$category), 'adjmcsc',
-                ifelse(grepl('allmcmc',df$variable), 'allmcmc',
-                   ifelse(grepl('allscsc',df$category), 'allscsc',
-                ifelse(grepl('allmcsc',df$category), 'allmcsc', 'unknown'
-                ))))))
+    df$hbcolor <- ifelse(grepl('adjmcmc',df$variable), 'adj_mcmc',
+                   ifelse(grepl('adjscsc',df$category), 'adj_scsc',
+                ifelse(grepl('adjmcsc',df$category), 'adj_mcsc',
+                ifelse(grepl('allmcmc',df$variable), 'nonadj_mcmc',
+                   ifelse(grepl('allscsc',df$category), 'nonadj_scsc','nonadj_mcsc')))))
     dfset=df[(df$keeps=='keep'),]
+    dfForce=dfset[(dfset$variable=='for-dispav'),]
+    #adding this new line here, to get a smoother control in different dataframe
     dfset=dfset[!(dfset$variable=='for-dispav'),]
-    #the above can be removed to add thin layout to force in bacground
-
     dfset=dfset[(dfset$avtype=='dispav'),]
-    print ('2')
     dfset$hbnature<- ifelse(grepl('p1',dfset$variable), 'partial',
                    ifelse(grepl('p2',dfset$variable), 'partial',   'unbiased'))
-    print ('3')
     dfset2=dfset[(dfset$hbnature=='partial'),]
     dfset1=dfset[(dfset$hbnature=='unbiased'),]
-    print ('4')
+    
+    #now here i do have finer control, upper and lowwer limits starts with 0 and 12,
+    #F in nN ranges from the 0 to 3, so divide original scale by the three and divide pN/1000 to get nN
+    #Moreover, now lets start specifying new axis, 
+    maxval=max(dfset1$value,na.rm = TRUE)
+    coeff=round(maxval / 3,digits=2)
     gg <- ggplot(data=dfset1[!is.na(dfset1$value),], aes(x=displacement,y=value,color=hbcolor))
-    gg <- gg + geom_line(size=0.6,alpha=0.5 ) + scale_color_manual(values=c("black","darkgreen","blue","gray","green","lightblue"))
-    gg <- gg + facet_wrap(~polymer,ncol=1) 
+    gg <- gg + geom_line(size=0.4,alpha=0.8 )
+    gg <- gg + geom_line(data=dfForce[!is.na(dfForce$value),], aes(x=displacement, y=(value / 1000)*coeff), size=0.1, alpha=0.8)
+    gg <- gg + scale_color_manual(values=c("black","darkgreen","blue","red","gray","green","lightblue"))
+    gg <- gg + scale_y_continuous(name='Hb_count',sec.axis = sec_axis(~./coeff, name="Force (nN)")) 
     gg <- gg + scale_x_continuous(name="Displacement (Ang)",breaks=seq(-2,25,4))
+    gg <- gg + facet_wrap(~polymer,ncol=1)
     gg <- gg + theme (strip.text.x = element_text(size = 7),axis.text.x = element_text( hjust = 1, size = 7, angle = 45),axis.text.y = element_text( hjust = 1, size = 7), legend.position="top", panel.background = element_rect(fill = "white", colour = "grey50"),panel.grid.major = element_line(colour = "grey90"),panel.grid.minor = element_line(colour = "grey95",size = 0.25))
-    gg <- gg + guides(colour = guide_legend(override.aes = list(size=5)))
-    gg <- gg + ylab("hbcount")
-    ggsave(filename = paste0(outapp,'unbiased.pdf'),gg, height = 8, width= 3)
+    gg <- gg + guides(colour = guide_legend(override.aes = list(size=1)))
+    #gg <- gg + ylab("hbcount")
+    ggsave(filename = paste0(outapp,'unbiased.pdf'),gg, height = 10, width= 5)
     }
-    print ('5')
-#dfgen<-dfgen[!(dfgen$color=="ra20" | dfgen$color=="ra250"),]
+
 dfgen=dfgen[!(dfgen$polymer=='polyala_constrained') & !(dfgen$polymer=='polygly_constrained') & !(dfgen$polymer=='polyisoleucine_server191') & !(dfgen$polymer=='polyisoleucine_78') & !(dfgen$polymer=='polyisoleucine_turing'),]
 dfgen=dfgen[!(dfgen$category=='start') & !(dfgen$category=='end'),]
-print ('6')
-plotcombined(dfgen,'hb',paste0(out,'hbcombined_facet'))
-plotallad(dfgen,'hbadj',paste0(out,'hbadjacent_facet'))
-plotallad(dfgen,'hball',paste0(out,'hball_facet'))
+#removing the extra polymers 
+#and start end remarks
+#print ('6')
+plotcombined(dfgen,'hb',paste0(out,'hbcombined_vericalStack'))
+#above function will only deal with the hbond categories (mc sc and combination) other than partial views,
+#it will plot both the ajdcanet and non adjacnet with the running average in form of diapav
+plotallad(dfgen,'hbadj',paste0(out,'hbAdj_verticalStack'))
+plotallad(dfgen,'hball',paste0(out,'hbNonAdj_verticalStack'))
+
+
+#Rscript vsdisp_hbondcount.R ../menton_set/ ../plots/new4sep/
